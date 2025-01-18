d65addeda190b070d3ca15edb4243610a4008bed
Umair Shaikh <imumairshaikh07@gmail.com>
2025-01-18 12:13:45 +0530

    made changes

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -36,6 +36,7 @@ const listItems = [
 
 const ProductCards = () => {
   const combinedProducts = [...data, ...newArrivals, ...bestSellers];
+  const [originalData] = useState(data);
   const [productData, setProductData] = useState(combinedProducts);
   const [inputValue, setInputValue] = useState("");
   const route = useLocation();
@@ -46,6 +47,8 @@ const ProductCards = () => {
 
   const [isOpen, setIsOpen] = useState(false);
   const [isPriceOpen, setIsPriceOpen] = useState(false);
+
+  const dataToMap = route.pathname === "/" ? originalData : productData;
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -216,107 +219,103 @@ const ProductCards = () => {
 
       {/* Products */}
       <div className="grid lg:gap-10 sm:gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 px-4 lg:px-16">
-        {productData.length > 0 ? (
-          productData.map((item) => (
+        {dataToMap.map((item) => (
+          <div
+            key={item.id}
+            className="relative bg-white-400 border border-gray-300 shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 flex flex-col justify-between gap-5"
+          >
+            {/* Discount Badge */}
+            {item.discount > 0 ? (
+              <div className="absolute top-0 left-0 bg-green-400 text-white-400 text-xs font-bold px-3 py-1 rounded-br-lg">
+                {item.discount}% OFF
+              </div>
+            ) : (
+              <div className="absolute top-0 left-0 bg-red-500 text-white-400 text-xs font-bold px-3 py-1 rounded-br-lg">
+                NO OFF
+              </div>
+            )}
+
+            {/* Product Image */}
             <div
-              key={item.id}
-              className="relative bg-white-400 border border-gray-300 shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 flex flex-col justify-between gap-5"
+              className="relative cursor-pointer group"
+              onClick={() => navigate(`/products-details/${item.id}`)}
             >
-              {/* Discount Badge */}
-              {item.discount > 0 ? (
-                <div className="absolute top-0 left-0 bg-green-400 text-white-400 text-xs font-bold px-3 py-1 rounded-br-lg">
-                  {item.discount}% OFF
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-44 m-auto mt-10 h-36 object-cover transition-transform duration-300 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+            </div>
+
+            {/* Product Details */}
+            <div className="flex-grow flex flex-col justify-between p-4">
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 truncate">
+                  {item.name}
+                </h3>
+                <div className="mt-2 flex items-center justify-between">
+                  <p className="text-sm text-gray-600">
+                    <span className="line-through text-red-500">
+                      ₹{item.price}
+                    </span>
+                    <span className="ml-2 text-gray-800 font-medium">
+                      ₹{item.discountedPrice} / {item.unit[0]}
+                    </span>
+                  </p>
+                </div>
+
+                {/* Star Rating */}
+                <div className="flex items-center mt-2">
+                  {Array.from({ length: 5 }, (_, index) => (
+                    <svg
+                      key={index}
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-5 w-5 ${
+                        index < item.reviews
+                          ? "text-yellow-400"
+                          : "text-gray-300"
+                      }`}
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 .587l3.668 7.572L24 9.748l-6 5.849 1.416 8.268L12 18.896l-7.416 4.969L6 15.597 0 9.748l8.332-1.589z" />
+                    </svg>
+                  ))}
+                  <span className="ml-2 text-gray-600 text-sm">
+                    ({item.reviews})
+                  </span>
+                </div>
+              </div>
+
+              {/* Add to Cart Button */}
+              {user ? (
+                <div className="lg:mt-10 sm:mt-5 mb-2">
+                  <button
+                    className="w-full bg-green-400 text-white-400 py-2 rounded-md text-sm font-medium hover:bg-green-600 transition-colors"
+                    onClick={() => {
+                      addToCart(item);
+                      handleSuccess(`${item.name} added to Cart`);
+                    }}
+                  >
+                    Add to Cart
+                  </button>
                 </div>
               ) : (
-                <div className="absolute top-0 left-0 bg-red-500 text-white-400 text-xs font-bold px-3 py-1 rounded-br-lg ">
-                  NO OFF
+                <div className="lg:mt-10 sm:mt-5 mb-2">
+                  <button
+                    className="w-full bg-green-400 text-white-400 py-2 rounded-md text-sm font-medium hover:bg-green-600 transition-colors"
+                    onClick={() => {
+                      handleError("Login to continue :(");
+                    }}
+                  >
+                    Add to Cart
+                  </button>
                 </div>
               )}
-
-              {/* Product Image */}
-              <div
-                className="relative cursor-pointer group"
-                onClick={() => navigate(`/products-details/${item.id}`)}
-              >
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="w-44 m-auto mt-10 h-36 object-cover transition-transform duration-300 group-hover:scale-105"
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-              </div>
-
-              {/* Product Details */}
-              <div className="flex-grow flex flex-col justify-between p-4">
-                <div>
-                  <h3 className="text-lg font-semibold text-gray-800 truncate">
-                    {item.name}
-                  </h3>
-                  <div className="mt-2 flex items-center justify-between">
-                    <p className="text-sm text-gray-600">
-                      <span className="line-through text-red-500">
-                        ₹{item.price}
-                      </span>
-                      <span className="ml-2 text-gray-800 font-medium">
-                        ₹{item.discountedPrice} / {item.unit[0]}
-                      </span>
-                    </p>
-                  </div>
-                  {/* Star Rating */}
-                  <div className="flex items-center mt-2">
-                    {Array.from({ length: 5 }, (_, index) => (
-                      <svg
-                        key={index}
-                        xmlns="http://www.w3.org/2000/svg"
-                        className={`h-5 w-5 ${
-                          index < item.reviews
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .587l3.668 7.572L24 9.748l-6 5.849 1.416 8.268L12 18.896l-7.416 4.969L6 15.597 0 9.748l8.332-1.589z" />
-                      </svg>
-                    ))}
-                    <span className="ml-2 text-gray-600 text-sm">
-                      ({item.reviews})
-                    </span>
-                  </div>
-                </div>
-                {/* Add to Cart Button */}
-                {user ? (
-                  <div className="lg:mt-10 sm:mt-5 mb-2">
-                    <button
-                      className="w-full bg-green-400 text-white-400 py-2 rounded-md text-sm font-medium hover:bg-green-600 transition-colors"
-                      onClick={() => {
-                        addToCart(item);
-                        handleSuccess(`${item.name} added to Cart`);
-                      }}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                ) : (
-                  <div className="lg:mt-10 sm:mt-5 mb-2">
-                    <button
-                      className="w-full bg-green-400 text-white-400 py-2 rounded-md text-sm font-medium hover:bg-green-600 transition-colors"
-                      onClick={() => {
-                        handleError(`Login to continue :(`);
-                      }}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                )}
-              </div>
             </div>
-          ))
-        ) : (
-          <p className="text-center text-red-500 text-lg">
-            No products found :(
-          </p>
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
